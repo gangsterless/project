e83af7954f940befffc354692e71e1ed6e858a27
zhanghahaha <5203161+zhanghahaxixi@user.noreply.gitee.com>
2019-10-16 20:38:51 +0800

    增加了几个常量，并且常量都大写⑧

--- Utils/ConstValues.py
+++ Utils/ConstValues.py
@@ -1,3 +1,7 @@
 #人类最大浏览量
+import time
 MAX_HUMAN_VISIT_TIMES = 2000
 BEHAVIOR_MAP = {0:"visit",1:"colletion",2:"cart",3:"buy"}
+PREDICTDAY = time.strptime('2014-12-18 00', "%Y-%m-%d %H")
+STARTTIME = time.strptime("2014-12-14 00", "%Y-%m-%d %H")
+ENDTIME  = time.strptime("2014-12-16 23", "%Y-%m-%d %H")
