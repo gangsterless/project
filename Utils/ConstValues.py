3add5c41678833755fda104e237e114c54dca901
zhanghahaha <5203161+zhanghahaxixi@user.noreply.gitee.com>
2019-10-16 22:48:09 +0800

    增加一些常量

--- Utils/ConstValues.py
+++ Utils/ConstValues.py
@@ -3,5 +3,5 @@ import time
 MAX_HUMAN_VISIT_TIMES = 2000
 BEHAVIOR_MAP = {0:"visit",1:"colletion",2:"cart",3:"buy"}
 PREDICTDAY = time.strptime('2014-12-18 00', "%Y-%m-%d %H")
-STARTTIME = time.strptime("2014-12-14 00", "%Y-%m-%d %H")
-ENDTIME  = time.strptime("2014-12-16 23", "%Y-%m-%d %H")
+TRAINSTARTTIME = time.strptime("2014-12-14 00", "%Y-%m-%d %H")
+TRAINENDTIME  = time.strptime("2014-12-16 23", "%Y-%m-%d %H")
